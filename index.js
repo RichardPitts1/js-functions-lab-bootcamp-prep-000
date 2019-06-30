9295c4fb14565dafcbf2d65af27e361b4daed9ff
Richard Pitts <richardepitts@gmail.com>
2019-06-30 16:11:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,20 @@
 // function happyHolidaysTo(holiday, name) {
 //   return `Happy ${holiday}, ${name}!`
 // }
+
+
+function happyHolidays() {
+  return "happy Holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `happy holidays, ${name}!`
+}
+
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} until ${holiday}!`
+}
