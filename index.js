e42defcef36001650797afc32d16b7a90b9843b4
Richard Pitts <richardepitts@gmail.com>
2019-06-30 16:08:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // write your code below!
-function happyHolidays() {
-  return "Happy holidays!"
-}
+// function happyHolidays() {
+//   return "Happy holidays!"
+// }
 
-function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
-}
+// function happyHolidaysTo(name) {
+//   return "Happy holidays, ${name}!"
+// }
 
-function happyHolidaysTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
-}
+// function happyHolidaysTo(holiday, name) {
+//   return `Happy ${holiday}, ${name}!`
+// }
